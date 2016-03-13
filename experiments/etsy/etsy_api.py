c884f41f2138ca9779a2472fcd6212d722471014
Martin Laprise <mlaprise@gmail.com>
2016-03-12 20:55:34 -0500

    add etsy experiment

---
+++ experiments/etsy/etsy_api.py
@@ -0,0 +1,40 @@
+import urllib
+import requests
+
+
+class Etsy(object):
+    '''Simple wrapper around Etsy API.'''
+
+    def __init__(self, etsy_apikey, etsy_secret):
+        self.base_url = 'https://openapi.etsy.com/v2'
+        self.apikey = etsy_apikey
+        self.secret = etsy_secret
+
+
+    def get_listing(self):
+        '''Get the most recent active listings on Etsy.
+
+        :return: The raw JSON response.
+        '''
+        endpoint= '/listings/active'
+        req_url = '{}{}?api_key={}'.format(self.base_url, endpoint, self.apikey)
+        return requests.get(req_url).json()
+
+
+    def get_shop_listing(self, shop_id, page=1, limit=100, fields=['title', 'description']):
+        '''Get the listing of specific shop.
+        :param shop_id: Unique id of of the shop.
+        :param page: Pagination.
+        :param limit: Number of item per page.
+        :param fields: Fields to include in the result.
+        :return: The raw JSON response.
+        '''
+        endpoint = '/shops/{}/listings/active'.format(shop_id)
+        args = {'page': page,
+                'limit': limit}
+
+        if fields != 'all':
+            args['fields'] = ','.join(fields)
+
+        req_url = '{}{}?api_key={}&{}'.format(self.base_url, endpoint, self.apikey, urllib.urlencode(args))
+        return requests.get(req_url).json()
---
+++ experiments/etsy/etsy_stats.py
@@ -0,0 +1,74 @@
+import nltk
+
+from collections import defaultdict
+from operator import itemgetter
+from nltk import sent_tokenize
+from nltk.tokenize import RegexpTokenizer
+from etsy_api import Etsy
+
+EN_STOPWORDS = set(nltk.corpus.stopwords.words())
+EN_STOPWORDS = EN_STOPWORDS.union(set(['etsy', 'www', 'http', 'https', 'shop']))
+
+ETSY_APIKEY = 'defmtlk9o2c64r3587c08qza'
+ETSY_SECRET = 'i5xax0ccnn'
+
+toker = RegexpTokenizer(r'\w{3,}')
+
+etsy_api = Etsy(ETSY_APIKEY, ETSY_APIKEY)
+
+
+def items_listing_generator(shop_id):
+    '''Fetch all items title and description for a specific etsy shop.
+
+    :param shop_id: Etsy shop id
+    '''
+    page = 1
+    while page:
+        response = etsy_api.get_shop_listing(shop_id, page=page)
+        page = response['pagination']['next_page']
+        for item in response['results']:
+            yield item['description'] + " " + item['title']
+
+
+def unigram_generator(docs):
+    '''Iterate through a list of document and yield token.
+
+    :param docs: A list of documents string.
+    '''
+    for full_content in docs:
+        for sent in nltk.sent_tokenize(full_content):
+            tokenized_content =  [t.lower() for t in toker.tokenize(sent) if t.lower() not in EN_STOPWORDS]
+            for tok in tokenized_content:
+                yield tok
+
+
+def get_top_terms(shop_items, top_n = 5):
+    '''Get the top terms (unigram) for a Etsy shop.
+
+    :param shop_items: List of all the shop items.
+    :param top_n: Return the top_n terms.
+    :return: List of top terms. Each item is a tuple (term, score)
+    '''
+    terms_freq = defaultdict(int)
+    for unigram in unigram_generator(shop_items):
+        terms_freq[unigram] += 1
+    top_terms = sorted(terms_freq.items(), key=itemgetter(1), reverse=True)[:5]
+    return top_terms
+
+
+if __name__ == "__main__":
+    etsy_shops = ['laplumeblanche',
+                  'LesNanaseries',
+                  'HoneyCanada',
+                  'ParasolsbyDesign',
+                  'ShinySteelDrums',
+                  'CakeInTheMorn',
+                  'PicasoLab',
+                  'bertiescloset',
+                  'TheComposersPen',
+                  'SupremerStudio']
+
+    for shop_id in etsy_shops:
+        print "\nEtsy Shop: {}".format(shop_id)
+        shop_items = items_listing_generator(shop_id)
+        print get_top_terms(shop_items)
